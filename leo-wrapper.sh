97d0762b1badb0b6baffeb2a80fab4adf625dcc8
yves <steve.harrison@gmx.net>
2013-08-21 11:14:21 +0200

    refactor and add support for multiple provers

--- leo-wrapper.sh
+++ leo-wrapper.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
+
+# only out put header
+if [[ -z $1 ]]; then
+    echo "problem, realtime, usertime, status"
+    exit 0
+fi
+
+
 LEO_OPTS=${*:2}
 FILE=$1
 PROBLEM=$(basename $1)
 
+
 STD_ERR_FILE=$(mktemp)
 STD_OUT_FILE=$(mktemp)
 TIME_STR=$({ TIMEFORMAT='%R, %U'; time timeout $((TIMELIMIT + 5)) leo ${LEO_OPTS} ${FILE} >${STD_OUT_FILE} 2>${STD_ERR_FILE}; } 2>&1)
+TIMEOUT_RETURN=$?
+
+echo ">>> prover std_err for ${FILE} >>>" 1>&2
 cat ${STD_ERR_FILE} 1>&2
+echo ">>>>>>" 1>&2
+
 
 
 # handle szs-status
 SZS_STATUS=$(grep -m 1 -o "^% SZS status [[:alpha:]]*"  ${STD_OUT_FILE})
+[[ -z ${SZS_STATUS} && ${TIMEOUT_RETURN} -eq 124 ]] && SZS_STATUS="Timeout"
 [[ -z ${SZS_STATUS} ]] && SZS_STATUS="Error"
 
 SZS_STATUS=${SZS_STATUS#"% SZS status "}
--- test.sh
+++ test.sh
@@ -19,6 +19,34 @@ function log_info() {
     echo $1
 }
 
+function load_config() {
+    source $1 || exit_with_reason "Unable to find config file '$1'"
+    CONFIG_FILE=$1
+
+    [[ -n ${TPTP_VERSION} ]] || TPTP_VERSION="5.5.0"
+    [[ -n ${FO_PROVERS} ]] || FO_PROVERS="E-1.8"
+    [[ -n ${TIMELIMIT} ]] || TIMELIMIT=30
+    [[ -n ${LEO_OPTS} ]] || LEO_OPTS="-f e"
+    [[ -n ${GIT_COMMIT} ]] || exit_with_reason "Need GIT_COMMIT to checkout code"
+    [[ -n ${TPTP_PROBLEMS} ]] || exit_with_reason "Need FILES to contain at least one test file"
+    [[ -n ${APPEND_OPTS} ]] || APPEND_OPTS=""
+}
+
+function generate_opts() {
+    LEO_OPTS=""
+    for prover in "${FO_PROVERS[@]}"; do
+        case ${prover} in
+            E-*)
+                LEO_OPTS="${LEO_OPTS} -f e --atp e=${prover}/PROVER/eprover"
+                ;;
+            SPASS-*)
+                LEO_OPTS=" ${LEO_OPTS} -f spass --atp spass=${prover}/SPASS"
+                ;;
+        esac
+    done
+    LEO_OPTS="${LEO_OPTS} -t ${TIMELIMIT} ${APPEND_OPTS}"
+}
+
 # continue if last run not done
 if [[ -L ${CURRENT_LINK} ]]; then
     log_info "Continue last run"
@@ -27,46 +55,35 @@ if [[ -L ${CURRENT_LINK} ]]; then
 
 fi
 
-
-# load and check config
-source $1 || exit_with_reason "Unable to find config file '$1'"
-CONFIG_FILE=$1
+load_config $1
+generate_opts
 
 RESULT_PREFIX="results/$(date +%Y%m%d_%H%M%S)"
 RESULT_DIR="${PWD}/${RESULT_PREFIX}"
+PROFILE="${RESULT_DIR}/profile"
+
 mkdir -p ${RESULT_DIR}
 ln ${RESULT_DIR} -s ${CURRENT_LINK}
 ln ${RESULT_DIR} -sf ${LAST_LINK}
-
-
-[[ -n ${E_VERSION} ]] || E_VERSION=1.8
-[[ -n ${TPTP_VERSION} ]] || TPTP_VERSION=5.5.0
-[[ -n ${GIT_COMMIT} ]] || exit_with_reason "Need GIT_COMMIT to checkout code"
-[[ -n ${TPTP_PROBLEMS} ]] || exit_with_reason "Need FILES to contain at least one test file"
-[[ -n ${TIMELIMIT} ]] || TIMELIMIT=30
-[[ -n ${LEO_OPTS} ]] || LEO_OPTS="-f e"
-
-
-LEO_OPTS="${LEO_OPTS} -t ${TIMELIMIT} --atp e=E-${E_VERSION}/PROVER/eprover"
-
-PROFILE="${RESULT_DIR}/profile"
 cp ${CONFIG_FILE} ${RESULT_DIR}/config
 
+# generate problem list
 TARGETS=""
-
 for FILE in ${TPTP_PROBLEMS}; do
-    TARGETS="${RESULT_PREFIX}/${FILE}.cvs ${TARGETS}"
+    TARGETS="${RESULT_PREFIX}/${FILE}.csv ${TARGETS}"
 done
 
 cat > ${PROFILE} <<EOF
 export PATH := leo-git-${GIT_COMMIT}/bin:\$(PATH)
 export TPTP := TPTP-v${TPTP_VERSION}
 
-${RESULT_PREFIX}/summery.cvs: ${TARGETS}
-	cat \$^ > \$@
+${RESULT_PREFIX}/summary.csv: ${TARGETS}
+	./leo-wrapper.sh > \$@
+	cat \$^ >> \$@
 	rm ${CURRENT_LINK}
 
-${RESULT_PREFIX}/%.p.cvs: TPTP-v${TPTP_VERSION} leo-git-${GIT_COMMIT} E-${E_VERSION}
+
+${RESULT_PREFIX}/%.p.csv: TPTP-v${TPTP_VERSION} ${FO_PROVERS}
 	mkdir -p \$(dir \$@)
 	./leo-wrapper.sh \$(TPTP)/Problems/\$*.p ${LEO_OPTS} > \$@
 EOF
